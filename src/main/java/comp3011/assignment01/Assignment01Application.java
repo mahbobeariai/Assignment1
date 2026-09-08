4639c263f2cc655ad6b08c5fbfb45f3fe2f55742
Mahboba Ariai <mahboba.ariai@student.adelaide.edu.au>
2026-09-08 17:08:44 +0930

    Created Spring Boot project

---
+++ src/main/java/comp3011/assignment01/Assignment01Application.java
@@ -0,0 +1,13 @@
+package comp3011.assignment01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment01Application.class, args);
+	}
+
+}
---
+++ src/test/java/comp3011/assignment01/Assignment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3011.assignment01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
