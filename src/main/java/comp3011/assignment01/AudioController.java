f435769cc87ef388617e5f37bf5ad4294ef76796
Mahboba Ariai <mahboba.ariai@student.adelaide.edu.au>
2026-09-10 16:33:54 +0930

    Connected AudioController to TranscriptionService

--- src/main/java/comp3011/assignment01/AudioController.java
+++ src/main/java/comp3011/assignment01/AudioController.java
@@ -6,6 +6,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api")
 public class AudioController {
+    private final TranscriptionService transcriptionService;
+
+    public AudioController(TranscriptionService transcriptionService) {
+        this.transcriptionService = transcriptionService;
+    }
 
     @PostMapping("/audio")
     public String receiveAudio(@RequestParam("audio") MultipartFile audio) {
---
+++ src/main/java/comp3011/assignment01/TranscriptionService.java
@@ -0,0 +1,8 @@
+package comp3011.assignment01;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TranscriptionService {
+
+}
