44accf04dfc6f3dcd985a255e6f6e45ffd05219f
Mahboba Ariai <mahboba.ariai@student.adelaide.edu.au>
2026-09-14 11:03:22 +0930

    Added test for multiple requests

--- src/test/java/comp3011/assignment01/Assignment01ApplicationTests.java
+++ src/test/java/comp3011/assignment01/Assignment01ApplicationTests.java
@@ -2,12 +2,54 @@ package comp3011.assignment01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Assignment01ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void handlesManyRequestsAtTheSameTime() {
 
+        HttpClient client = HttpClient.newHttpClient();
+
+        List<CompletableFuture<HttpResponse<String>>> requests =
+                new ArrayList<>();
+        for (int i = 0; i < 250; i++) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(
+                            "http://localhost:" + port + "/api/v1/admin/uptime"))
+                    .GET()
+                    .build();
+
+            requests.add(
+                    client.sendAsync(
+                            request,
+                            HttpResponse.BodyHandlers.ofString()
+                    )
+            );
+        }
+        CompletableFuture.allOf(
+                requests.toArray(new CompletableFuture[0])
+        ).join();
+
+        for (CompletableFuture<HttpResponse<String>> request : requests) {
+            assertEquals(200, request.join().statusCode());
+        }
+    }
 }
